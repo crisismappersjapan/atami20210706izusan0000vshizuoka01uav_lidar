039f11abbac59535f1188f852b4333652679822f
Taichi FURUHASHI <mapconcierge@gmail.com>
2021-07-11 17:29:39 +0900

    Add uav lidar data set

---
+++ docs/pointclouds/Izusan_UAV-LiDAR_20210706/cloud.js
@@ -0,0 +1,29 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 105572358,
+    "boundingBox": {
+        "lx": 52029.246,
+        "ly": -98643.17199999999,
+        "lz": -0.712,
+        "ux": 53553.059,
+        "uy": -97119.359,
+        "uz": 1523.101000000002
+    },
+    "tightBoundingBox": {
+        "lx": 52029.246,
+        "ly": -98643.17199999999,
+        "lz": -0.712,
+        "ux": 53553.059,
+        "uy": -97162.38299999999,
+        "uz": 431.464
+    },
+    "pointAttributes": [
+        "POSITION_CARTESIAN",
+        "COLOR_PACKED"
+    ],
+    "spacing": 13.19660758972168,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
